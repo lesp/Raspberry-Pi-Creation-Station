cfa7670c05e894671bccca6e98cd96b7da64f330
Les Pounder <lespounder@gmail.com>
2015-06-16 19:58:29 +0100

    First commit

---
+++ drives.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#Starter for a script to detect USB SD card readers on laptop and then use dcfldd to write image to multiple cards
+
+echo "Let's see what drives are available"
+dev="/dev/sd"
+
+for drive in b c d e f
+do
+    #echo $dev$drive
+    df | grep -o $dev$drive
+done
+
+#dcfldd if=./hackpool.img of=/dev/sdb of=/dev/sdc of=/dev/mmcblk0 sizeprobe=if
+
+
+
+
